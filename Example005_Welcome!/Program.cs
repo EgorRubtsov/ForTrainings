171da3568cac6b06229635bf0521e5271a038e06
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-16 03:43:06 +0700

    Example005

---
+++ Example005_Welcome!/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please, enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray! This is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
