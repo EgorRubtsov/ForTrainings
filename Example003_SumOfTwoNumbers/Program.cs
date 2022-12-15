938199a1833fd31025d12df2c3fd1ae226e023f7
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-16 01:56:41 +0700

    Example))3_Sum

---
+++ Example003_SumOfTwoNumbers/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+int sum = (a + b);
+Console.WriteLine(sum);
+
