ba26b3c97f2a86db7de5acd3e26244f0b16dbb4b
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-16 03:14:37 +0700

    Example 004_DIV

---
+++ Example004_div/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter first number: ");
+double number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+double number2 = int.Parse(Console.ReadLine());
+double div = number1 / number2;
+Console.WriteLine(div);
